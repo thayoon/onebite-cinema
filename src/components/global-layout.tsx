10ca3a023f5e4a612618895cacef12326d306902
thayoon <thagyun@naver.com>
2025-03-27 23:04:58 +0900

    day4 layout

---
+++ src/components/global-layout.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+import style from "./global-layout.module.css";
+import Link from "next/link";
+
+export default function GlobalLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className={style.container}>
+      <header className={style.header}>
+        <Link href={"/"}>ONEBITE CINEMA</Link>
+      </header>
+      <main>{children}</main>
+    </div>
+  );
+}
