ad1dfc1c293491e9f2421c13f6b969bac96b3d9e
thayoon <thagyun@naver.com>
2025-04-19 18:51:42 +0900

    day8: ISR

--- src/pages/movie/[id].tsx
+++ src/pages/movie/[id].tsx
@@ -3,6 +3,7 @@ import style from "./[id].module.css";
 import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import fetchOneMovie from "@/lib/fetch-one-movie";
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 export const getStaticPaths = () => {
   return {
@@ -33,8 +34,25 @@ export default function Page({
   movie,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   const router = useRouter();
-  if (router.isFallback) return "로딩중...";
+  if (router.isFallback) {
+    return (
+      <>
+        <Head>
+          <title>한 입 시네마</title>
+          <meta property="og:image" content="./thumbnail.png" />
+          <meta property="og:title" content="한 입 시네마" />
+          <meta
+            property="og:description"
+            content="한 입 시네마에 등록된 영화들을 만나보세요."
+          />
+        </Head>
+        <div>로딩중...</div>
+      </>
+    );
+  }
+
   if (!movie) return "문제가 발생했습니다. 다시 시도하세요.";
+
   const {
     title,
     releaseDate,
@@ -47,26 +65,34 @@ export default function Page({
   } = movie;
 
   return (
-    <div className={style.container}>
-      <div
-        className={style.cover_img_container}
-        style={{ backgroundImage: `url('${posterImgUrl}')` }}
-      >
-        <img src={posterImgUrl} alt={title} />
-      </div>
-      <div className={style.info}>
-        <h1>{title}</h1>
-
-        <div>
-          {releaseDate} | {genres.join(", ")} | {runtime}분
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta property="og:image" content={posterImgUrl} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+      </Head>
+      <div className={style.container}>
+        <div
+          className={style.cover_img_container}
+          style={{ backgroundImage: `url('${posterImgUrl}')` }}
+        >
+          <img src={posterImgUrl} alt={title} />
         </div>
+        <div className={style.info}>
+          <h1>{title}</h1>
 
-        <div>{company}</div>
+          <div>
+            {releaseDate} | {genres.join(", ")} | {runtime}분
+          </div>
+
+          <div>{company}</div>
+        </div>
+        <div className={style.description}>
+          <h3>{subTitle}</h3>
+          <div>{description}</div>
+        </div>
       </div>
-      <div className={style.description}>
-        <h3>{subTitle}</h3>
-        <div>{description}</div>
-      </div>
-    </div>
+    </>
   );
 }
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -5,6 +5,7 @@ import style from "./index.module.css";
 import fetchMovies from "@/lib/fetch-movies";
 import { useRouter } from "next/router";
 import { MovieData } from "@/types";
+import Head from "next/head";
 
 export default function Page() {
   const [movies, setMovies] = useState<MovieData[]>([]);
@@ -25,6 +26,15 @@ export default function Page() {
 
   return (
     <div className={style.container}>
+      <Head>
+        <title>한 입 시네마 - 검색 결과</title>
+        <meta property="og:image" content="./thumbnail.png" />
+        <meta property="og:title" content="한 입 시네마 - 검색 결과" />
+        <meta
+          property="og:description"
+          content="한 입 시네마에 등록된 영화들을 만나보세요."
+        />
+      </Head>
       {movies.map((movie) => (
         <MovieItem key={movie.id} {...movie} />
       ))}
